4ca3374c5049d7e7115bbb801fdd4454ad10a3dc
sskyy <skyking_H@hotmail.com>
2012-11-09 23:39:01 +0800

    重度重构中。

---
+++ application/modules/dsl/dsl.php
@@ -0,0 +1,19 @@
+<?php
+
+class dslModule{
+	public function __construct(){
+
+	}
+
+	public function extend_upper_module( &$moduleIns ){
+		if( isset( $moduleIns -> logic ){
+
+		});
+	}
+
+	public function extend_module_info(){
+		return "logic";
+	}
+}
+
+?>
---
+++ application/modules/dsl/info.php
@@ -0,0 +1,6 @@
+<?php
+
+$listen = array(
+	"system/upper_module_init" => "extend_upper_level_module",
+);
+?>
